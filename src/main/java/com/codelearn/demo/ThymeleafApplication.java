f59c27ac595964f6cef04e7c2f0ffc6e802a54f9
VuaVoDanh729 <48729858+VuaVoDanh729@users.noreply.github.com>
2020-12-28 21:45:37 +0700

    Update ThymeleafApplication.java

--- src/main/java/com/codelearn/demo/ThymeleafApplication.java
+++ src/main/java/com/codelearn/demo/ThymeleafApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThymeleafApplication {
+    
+    @RequestMapping("/")
+    String home() {
+      return "Hello World!";
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(ThymeleafApplication.class, args);
     }
